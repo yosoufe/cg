ec46df9f050bf41394ba859c7ee31b4bc0a70512
Yousof Ebneddin <yousofebneddin@yahoo.com>
2020-12-11 10:17:53 -0800

    section 2.11 rotation

--- main.cpp
+++ main.cpp
@@ -12,11 +12,14 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 
 GLuint VAO, VBO, shader, uniformModel;
+const float toRadians = 3.14159265f/180.0f;
 
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxOffset = 0.7f;
 float triIncrement = 0.005f;
+
+float curAngle = 0.0f;
 
 // Vertex Shader
 static const char* vShader = "\n\
@@ -27,7 +30,7 @@ uniform mat4 model;\n\
 \n\
 void main() \n\
 {\n\
-    gl_Position = model * vec4(0.4*pos.x , 0.4*pos.y, pos.z, 1.0);\n\
+    gl_Position = model * vec4(pos.x *0.4 , pos.y* 0.4, pos.z, 1.0);\n\
 }\n\
 ";
 
@@ -194,6 +197,12 @@ int main(){
             direction = !direction;
         }
 
+        curAngle += 1.0f;
+
+        if (curAngle >=360){
+            curAngle -= 360.0f;
+        }
+
         // clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -201,7 +210,8 @@ int main(){
         glUseProgram(shader);
         
         glm::mat4 model(1.0f);
-        model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+        model = glm::translate(model, glm::vec3(triOffset, 0, 0.0f));
+        model = glm::rotate(model, curAngle*toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
 
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
