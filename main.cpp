6ba8ce02ded000b23f0c19c5dc951d5a32b26a66
Yousof Ebneddin <yousofebneddin@yahoo.com>
2020-12-12 12:40:32 -0800

    section 2.15 indexed Draw and depth test

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel;
 const float toRadians = 3.14159265f/180.0f;
 
 bool direction = true;
@@ -59,14 +59,26 @@ void main() \n\
 
 void CreateTriangle()
 {
+    unsigned int indices[] ={
+        0, 3, 1,
+        1, 3, 2,
+        2, 3, 0,
+        0, 1, 2
+    };
+
     GLfloat vertices[] = {
         -1.0f, -1.0f, 0.0f,
+        0.0f,  -1.0f, 1.0f,
         1.0f , -1.0f, 0.0f,
         0.0f, 1.0f, 0.0f
     };
 
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -76,8 +88,9 @@ void CreateTriangle()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void addShader(GLuint theProgram, const char* shaderCode, GLenum shaderType){
@@ -184,6 +197,8 @@ int main(){
         return 1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     // setup viewport size
     glViewport(0,0, bufferWidth, bufferHeight);
 
@@ -226,12 +241,12 @@ int main(){
 
         // clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shader);
         
         glm::mat4 model(1.0f);
-        // model = glm::rotate(model, curAngle*toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::rotate(model, curAngle*toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
         // model = glm::translate(model, glm::vec3(triOffset, 0, 0.0f));
         model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
@@ -239,7 +254,11 @@ int main(){
 
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
         glUseProgram(0);
 
